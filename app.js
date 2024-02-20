981215871d56f2f767b0ece332947b3fc6e26db9
Mario Prieto <mariops99@outlook.com>
2024-02-20 22:58:21 +0100

    Leo todas las electrolineras y las formateo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const e = require('express');
+const express = require('express');
 const https = require('https');
 const parseString = require('xml2js').parseString;
 
@@ -17,8 +17,9 @@ https.get(url, (res) => {
         console.error(err);
       } else {
         const listadoElectrolineras = result['d2:payload']['egi:energyInfrastructureTable'][0];
-        const primeraElectrolinera = listadoElectrolineras['egi:energyInfrastructureSite'][0];
-        console.log(formatElectrolinera(primeraElectrolinera));
+        for (let electrolinera of listadoElectrolineras['egi:energyInfrastructureSite']) {
+          console.log(formatElectrolinera(electrolinera));
+        }
       }
     });
   });
@@ -32,7 +33,7 @@ function formatElectrolinera(electrolinera) {
     id: electrolinera['$']['id'],
     nombre: electrolinera['fac:name'][0]['com:values'][0]['com:value'][0]['_'],
     horario: electrolinera['fac:operatingHours'][0]['fac:label'],
-    lugar: electrolinera['egi:typeOfSite'][0],
+    lugar: electrolinera['egi:typeOfSite'],
     direccion: {
       calle: electrolinera['fac:locationReference'][0]['loc:_locationReferenceExtension'][0]['loc:facilityLocation'][0]['locx:address'][0]['locx:addressLine'][0]['locx:text'][0]['com:values'][0]['com:value'][0]['_'],
       codigoPostal: electrolinera['fac:locationReference'][0]['loc:_locationReferenceExtension'][0]['loc:facilityLocation'][0]['locx:address'][0]['locx:postcode'][0],
@@ -49,8 +50,10 @@ function formatElectrolinera(electrolinera) {
     cargadores: []
   }
 
+  //Me gustaría modificar los datos de la dirección (localidad, provincia, comunidad autónoma) ya que el string muestra Municipio: y el municipio que sea. Quitar el Municipio: y dejar solo el nombre del municipio.
+
   // Formatear el lugar de la electrolinera
-  switch(electrolinera['egi:typeOfSite'][0]) {
+  switch(electrolinera['egi:typeOfSite']) {
     case 'onstreet':
       electrolineraFormateada.lugar = 'En la calle';
       break;
@@ -70,8 +73,8 @@ function formatElectrolinera(electrolinera) {
       objetoConector = {
         tipo: conector['egi:connectorType'][0],
         potencia: conector['egi:maxPowerAtSocket'][0],
-        amperaje: conector['egi:maximumCurrent'][0],
-        voltaje: conector['egi:voltage'][0],
+        amperaje: conector['egi:maximumCurrent'],
+        voltaje: conector['egi:voltage'],
         formato: conector['egi:connectorFormat'][0],
       }
 
@@ -87,7 +90,7 @@ function formatElectrolinera(electrolinera) {
           objetoConector['tipo'] = 'CHAdeMO';
           break;
         default:
-          objetoConector['tipo'] = 'Desconocido';
+          objetoConector['tipo'] = conector['egi:connectorType'][0]; // Dejar el valor original mientras no conozcamos todos los tipos
           break;
       }
 
