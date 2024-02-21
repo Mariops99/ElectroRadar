6fcb343c840be750a85d6e2154b476a9ee616683
PiterDev <piterzdev@gmail.com>
2024-02-21 14:29:30 +0100

    tipos del jsdoc

---
+++ ts/jsdoc_app.js
@@ -0,0 +1,49 @@
+/**
+ * @typedef {{
+ *  id: string,
+ * nombre: string,
+ * horario: string,
+ * lugar: string,
+ * direccion: direccion,
+ * coordenadas: coordenadas,
+ * operador: string,
+ * activacion: string,
+ * cargadores: cargador[]
+ * }} electrolinera
+ */
+
+/**
+ * @typedef {{
+ * calle: string,
+ * codigoPostal: string,
+ * localidad: string,
+ * provincia: string,
+ * comunidadAutonoma: string
+ * }} direccion
+ *
+ */
+
+/**
+ * @typedef {{
+ * latitud: string,
+ * longitud: string
+ * }} coordenadas
+ */
+
+/**
+ * @typedef {{
+ * id: string,
+ * nombre: string,
+ * conectores: conector[]
+ * }} cargador
+ */
+
+/**
+ * @typedef {{
+ * tipo: string,
+ * formato: string,
+ * potencia: string,
+ * amperaje: string,
+ * voltaje: string
+ * }} conector
+ */
