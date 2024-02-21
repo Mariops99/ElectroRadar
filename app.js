cd745ec19f34d4e137890359cae317aa91db393b
Mario Prieto <mariops99@outlook.com>
2024-02-21 12:02:05 +0100

    Problemas menores resueltos

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ https.get(url, (res) => {
       if (err) {
         console.error(err);
       } else {
-        const listadoElectrolineras = result['d2:payload']['egi:energyInfrastructureTable'][0];
-        for (let electrolinera of listadoElectrolineras['egi:energyInfrastructureSite']) {
+        for (let electrolinera of result['d2:payload']['egi:energyInfrastructureTable'][0]['egi:energyInfrastructureSite']) {
           console.log(formatElectrolinera(electrolinera));
         }
       }
@@ -27,12 +26,11 @@ https.get(url, (res) => {
   console.error(err);
 });
 
-
 function formatElectrolinera(electrolinera) {
   const electrolineraFormateada = {
     id: electrolinera['$']['id'],
     nombre: electrolinera['fac:name'][0]['com:values'][0]['com:value'][0]['_'],
-    horario: electrolinera['fac:operatingHours'][0]['fac:label'],
+    horario: electrolinera['fac:operatingHours'][0]['fac:label'][0],
     lugar: electrolinera['egi:typeOfSite'],
     direccion: {
       calle: electrolinera['fac:locationReference'][0]['loc:_locationReferenceExtension'][0]['loc:facilityLocation'][0]['locx:address'][0]['locx:addressLine'][0]['locx:text'][0]['com:values'][0]['com:value'][0]['_'],
@@ -53,32 +51,41 @@ function formatElectrolinera(electrolinera) {
   //Me gustaría modificar los datos de la dirección (localidad, provincia, comunidad autónoma) ya que el string muestra Municipio: y el municipio que sea. Quitar el Municipio: y dejar solo el nombre del municipio.
 
   // Formatear el lugar de la electrolinera
-  switch(electrolinera['egi:typeOfSite']) {
-    case 'onstreet':
-      electrolineraFormateada.lugar = 'En la calle';
-      break;
-    default:
-      electrolineraFormateada.lugar = 'Desconocido';
-      break;
+  if(electrolinera['egi:typeOfSite'] === undefined) {
+    electrolineraFormateada.lugar = 'Desconocido';
+  } else {
+    switch(electrolinera['egi:typeOfSite'][0]) {
+      case 'onstreet':
+        electrolineraFormateada.lugar = 'En la calle';
+        break;
+      case  'openSpace':
+        electrolineraFormateada.lugar = 'Parking público';
+        break;
+      default:
+        electrolineraFormateada.lugar = electrolinera['egi:typeOfSite'][0]; // Dejar el valor original mientras no conozcamos todos los tipos
+        break;
+    }
   }
 
+  // Una electrolinera puede tener varios cargadores
   for (let cargador of electrolinera['egi:energyInfrastructureStation'][0]['egi:refillPoint']) {
     objetoCargador = {
         id: cargador['$']['id'],
         nombre: cargador['fac:name'][0]['com:values'][0]['com:value'][0]['_'],
-        conectores: []
+        conectores: [] 
     }
     
+    // Un cargador puede tener varios conectores
     for(let conector of cargador['egi:connector']) {
       objetoConector = {
         tipo: conector['egi:connectorType'][0],
-        potencia: conector['egi:maxPowerAtSocket'][0],
-        amperaje: conector['egi:maximumCurrent'],
-        voltaje: conector['egi:voltage'],
         formato: conector['egi:connectorFormat'][0],
-      }
+        potencia: conector['egi:maxPowerAtSocket'][0],
+        amperaje: conector['egi:maximumCurrent'] ? conector['egi:maximumCurrent'][0] : 'Desconocido',
+        voltaje: conector['egi:voltage'] ? conector['egi:voltage'][0] : 'Desconocido',
+      };
 
-      // Formatear el tipo de conector
+      // Formatear el tipo de cable del conector
       switch (conector['egi:connectorType'][0]) {
         case 'iec62196T2':
           objetoConector['tipo'] = 'Tipo 2';
@@ -90,7 +97,7 @@ function formatElectrolinera(electrolinera) {
           objetoConector['tipo'] = 'CHAdeMO';
           break;
         default:
-          objetoConector['tipo'] = conector['egi:connectorType'][0]; // Dejar el valor original mientras no conozcamos todos los tipos
+          objetoConector['tipo'] = conector['egi:connectorType'][0]; // Dejar el valor original mientras no conozcamos todos los tipos de cables de recarga
           break;
       }
 
@@ -103,16 +110,21 @@ function formatElectrolinera(electrolinera) {
           objetoConector['formato'] = 'Conector';
           break;
         default:
-          objetoConector['formato'] = 'Desconocido';
+          objetoConector['formato'] = conector['egi:connectorFormat'][0]; // Dejar el valor original mientras no conozcamos todos los formatos de conectores
           break;
       }
 
       // Formatear la potencia del conector
       objetoConector['potencia'] = parseInt(conector['egi:maxPowerAtSocket'][0]) / 1000 + 'kW';
 
-      objetoCargador['conectores'].push(objetoConector);
+      //Meter el objeto conector en el array de conectores
+      objetoCargador['conectores'].push(objetoConector);  
       }
+
+      //Meter el objeto cargador en el array de cargadores
       electrolineraFormateada['cargadores'].push(objetoCargador);
   }
+
+  // Devolver la electrolinera formateada
   return electrolineraFormateada;
 }
