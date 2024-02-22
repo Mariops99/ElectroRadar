e91470000ef3f8595a5b9128b4f64c7560b788b6
PiterDev <piterzdev@gmail.com>
2024-02-22 20:27:10 +0100

    electrolineras api cors solucionado

---
+++ src/pages/api/electrolineras.js
@@ -0,0 +1,31 @@
+export async function GET(_params, _request) {
+  const url =
+    "https://infocar.dgt.es/datex2/v3/miterd/EnergyInfrastructureTablePublication/electrolineras.xml";
+
+  const res = await fetch(url, {
+    headers: {
+      "cache-control": "no-cache",
+    },
+  });
+
+  if (!res.ok) {
+    return new Response(
+      JSON.stringify({ error: "Error al obtener las electrolineras" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  const data = await res.text();
+
+  return new Response(data, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
